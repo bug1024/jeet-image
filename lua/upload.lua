18d085d9013225b1e6702c445cef1beed53a9625
bug1024 <bug1024.com@gmail.com>
2017-04-10 22:50:09 +0800

    upload

--- lua/upload.lua
+++ lua/upload.lua
@@ -1,1 +1,31 @@
--- 图片上传
+-- 文件上传
+
+local upload = require "resty.upload"
+local cjson = require "cjson"
+
+local chunk_size = 4096
+
+local form, err = upload:new(chunk_size)
+if not form then
+    ngx.log(ngx.ERR, "failed to new upload: ", err)
+    ngx.exit(500)
+end
+
+form:set_timeout(1000)
+
+while true do
+    local typ, res, err = form:read()
+    if not typ then
+        ngx.say("failed to read: ", err)
+        return
+    end
+
+    ngx.say("read: ", cjson.encode({typ, res}))
+
+    if typ == "eof" then
+        break
+    end
+end
+
+local typ, res, err = form:read()
+ngx.say("read: ", cjson.encode({typ, res}))
